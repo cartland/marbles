832fe6cbb5576b29c402c813ba0dc92021dad4db
cartland <c.cartland@gmail.com>
2017-04-15 02:07:56 -0700

    Ball rolls around

---
+++ Marbles/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float forceMagnitude;
+
+	private Rigidbody player;
+
+	void Start() {
+		player = GetComponent<Rigidbody> ();
+	}
+	
+	void FixedUpdate() {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f,  moveVertical);
+		player.AddForce (movement * forceMagnitude);
+	}
+}
